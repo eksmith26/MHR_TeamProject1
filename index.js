8210ebacfa7486dc218aad2f61a9716cd44692a3
Ellie Smith <eksmith26@wisc.edu>
2024-02-25 14:14:20 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+document.getElementById("jobSecurityButton").addEventListener("click", function() {
+    window.location.href = "jobSecurity.html";
+  });
+
+document.getElementById("OTLongHoursButton").addEventListener("click", function() {
+    window.location.href = "longHours.html";
+  });
+
+document.getElementById("riskOfInjuryButton").addEventListener("click", function() {
+    window.location.href = "riskOfInjury.html";
+  });
+document.getElementById("indexButton").addEventListener("click", function() {
+    window.location.href = "index.html";
+  });
